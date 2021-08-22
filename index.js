fe147ab585e9a8c6924cf6509204defc565e809e
salhindi <calisaraalhindi@gmail.com>
2021-08-21 17:37:37 -0700

    post travel plans html and listener

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const endPoint = "http://localhost:3000/api/v1/travel_plans"
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
    getTravelPlans()  
+
+   const createTravelPlanForm = document.querySelector("#create-travel-plan-form")
+
+   createTravelPlanForm.addEventListener("submit", (e) => createFormHandler(e))
 })
+
+
 
 function getTravelPlans() {
     fetch(endPoint)
@@ -21,7 +29,88 @@ function getTravelPlans() {
 
                 document.querySelector('#travel-plan-container').innerHTML += travelPlanHTML
             
-        });
+        })
         
     }) 
+
 }
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector('#input-name').value
+    const startDateInput = document.querySelector('#input-start_date').value
+    const endDateInput = document.querySelector('#input-end_date').value
+    const destinationInput = document.querySelector('#destinations').value
+    const destinationId = parseInt(destinationInput)
+    postFetch(nameInput, startDateInput, endDateInput, destinationId)
+    
+}
+
+function postFetch(name, start_date, end_date, destination_id) {
+    console.log(name, start_date, end_date, destination_id)
+
+}
+// const travelPlanCreate = document.querySelector("#travel-plan-create")
+
+// travelPlanCreate.addEventListener('submit', function(e){
+//     const TRAVEL_PLANS_URL = 'http://localhost:3000/api/v1/travel_plans'
+//     e.preventDefault()
+//     debugger
+//     fetch(TRAVEL_PLANS_URL, {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/json",
+//             accept: "application/json"
+//         },
+//         body: JSON.stringify({
+//             travel_plan: {
+//                 name: travelPlanInputs[0].value,
+//                 start_date: travelPlanInputs[1].value,
+//                 end_date: travelPlanInputs[3].value,
+//                 destination: travelPlanInputs[4].value,
+//                } 
+                
+            
+//         })
+//     })
+//     const option = document.createElement("option")
+
+// option.text= "Text"
+
+// option.value = "myvalue"
+
+// const select = document.getElementById("selectDestination")
+
+// select.appendChild(option)
+
+// .then(res => res.json())
+// .then(function(object){
+//     if (object.message) {
+//         alert(object.message)
+//     }
+//     else {
+//         travelPlanMade(object)
+//     }
+// })
+
+// })
+
+// // function travelPlanMade(object){
+// //     travelPlans
+// // }
+
+
+
+// // var select = document.getElementById("selectDestination"); 
+// // // var options = ;
+
+
+// // // const destinations = destinations
+
+// // for(var i = 0; i < options.length; i++) {
+// //     var opt = options[i];
+// //     var el = document.createElement("option");
+// //     el.textContent = opt;
+// //     el.value = opt;
+// //     select.appendChild(el);
+// // }​
